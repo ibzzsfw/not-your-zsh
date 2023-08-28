637c5052f5e92b840d3ee1d2b635aae9fae21775
ibzzsfw <tap.pt33@gmail.com>
2023-08-28 23:26:27 +0700

    feat: more script

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
-# clone zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+clone_if_not_exists() {
+    dir=$1
+    repo_url=$2
+    if [ -d "$dir" ]; then
+        echo "[WARN] $dir exists"
+    else
+        echo "[INF] cloning for $dir"
+        git clone $repo_url $dir
+    fi
+}
 
-# clone zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+CUSTOM_ZSH=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+HOME_ZSH=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
 
-# clone powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+clone_if_not_exists "${CUSTOM_ZSH}/plugins/zsh-autosuggestions" "https://github.com/zsh-users/zsh-autosuggestions"
+clone_if_not_exists "${CUSTOM_ZSH}/plugins/zsh-syntax-highlighting" "https://github.com/zsh-users/zsh-syntax-highlighting.git"
+clone_if_not_exists "${HOME_ZSH}/themes/powerlevel10k" "https://github.com/romkatv/powerlevel10k.git"
 
-# reboot
 zsh
