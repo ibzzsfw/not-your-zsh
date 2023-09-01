3c9c1b20d459340d53d3d61a247955e9ecc198fb
ibzzsfw <tap.pt33@gmail.com>
2023-09-01 23:59:17 +0700

    feat: checking OS

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <img width="697" alt="Screenshot 2566-08-28 at 23 50 10" src="https://github.com/ibzzsfw/not-your-zsh/assets/68814521/3a3358e9-075f-4d25-a510-9930dfdb835b">
 
-> afaik, zsh supported on macOS and Linux only (UNIX based OS).
+> afaik, `zsh` supported on macOS and Linux only (UNIX based OS).
 
 Run this first to change the permissions of the script
 
--- install.sh
+++ install.sh
@@ -9,12 +9,57 @@ clone_if_not_exists() {
     fi
 }
 
+# function to check if zsh is installed, return boolean
+is_zsh_installed() {
+    if [ -x "$(command -v zsh)" ]; then
+        echo "[INF] zsh is installed"
+        return 0
+    fi
+    echo "[ERR] zsh is not installed"
+
+    return 1
+}
+
+# function to ask user to install zsh
+ask_install_zsh() {
+    echo "[INF] Do you want to install zsh? (Y/n)"
+    read -p "[INF] " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo "[INF] installing zsh"
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+            echo "[INF] macOS detected"
+            brew install zsh
+        elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+            echo "[INF] Linux detected"
+            sudo apt install zsh
+        fi
+    else
+        echo "[INF] skipping zsh installation"
+    fi
+}
+
 CUSTOM_ZSH=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 HOME_ZSH=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+
+# check if zsh is installed,
+# if not, ask user to install zsh
+# else, continue
+
+if ! is_zsh_installed; then
+    ask_install_zsh
+else
+    echo "[INF] zsh is installed"
+    echo "[INF] OSTYPE was detected as $OSTYPE"
+fi
 
 clone_if_not_exists "${CUSTOM_ZSH}/plugins/zsh-autosuggestions" "https://github.com/zsh-users/zsh-autosuggestions"
 clone_if_not_exists "${CUSTOM_ZSH}/plugins/zsh-syntax-highlighting" "https://github.com/zsh-users/zsh-syntax-highlighting.git"
 clone_if_not_exists "${HOME_ZSH}/themes/powerlevel10k" "https://github.com/romkatv/powerlevel10k.git"
 # add more plugins here ...
 
+echo "[INF] running zsh..."
+
 zsh
+
+echo "[INF] done"
