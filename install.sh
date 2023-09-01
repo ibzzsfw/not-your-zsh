f745ed57f306b14a62b3bd8404049f662efbf39f
ibzzsfw <tap.pt33@gmail.com>
2023-09-02 00:13:55 +0700

    feat: cd

--- install.sh
+++ install.sh
@@ -1,65 +1,77 @@
-clone_if_not_exists() {
-    dir=$1
-    repo_url=$2
+#!/bin/bash
+
+# Define constants
+CUSTOM_ZSH="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"
+HOME_ZSH="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+# Clone the repository if it does not exist in the directory
+function clone_if_not_exists {
+    local dir=$1
+    local repo_url=$2
     if [ -d "$dir" ]; then
         echo "[WARN] $dir exists"
     else
-        echo "[INF] cloning for $dir"
+        echo "[INF] Cloning for $dir"
         git clone $repo_url $dir
     fi
 }
 
-# function to check if zsh is installed, return boolean
-is_zsh_installed() {
+# Check if zsh is installed
+function is_zsh_installed {
     if [ -x "$(command -v zsh)" ]; then
         echo "[INF] zsh is installed"
         return 0
     fi
     echo "[ERR] zsh is not installed"
-
     return 1
 }
 
-# function to ask user to install zsh
-ask_install_zsh() {
+# Ask the user if they want to install zsh
+function ask_install_zsh {
     echo "[INF] Do you want to install zsh? (Y/n)"
     read -p "[INF] " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        echo "[INF] installing zsh"
-        if [[ "$OSTYPE" == "darwin"* ]]; then
-            echo "[INF] macOS detected"
-            brew install zsh
-        elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-            echo "[INF] Linux detected"
-            sudo apt install zsh
-        fi
+        install_zsh
     else
-        echo "[INF] skipping zsh installation"
+        echo "[INF] Skipping zsh installation"
     fi
 }
 
-CUSTOM_ZSH=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
-HOME_ZSH=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+# Install zsh
+function install_zsh {
+    echo "[INF] Installing zsh"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "[INF] macOS detected"
+        brew install zsh
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo "[INF] Linux detected"
+        sudo apt install zsh
+    fi
+}
 
-# check if zsh is installed,
-# if not, ask user to install zsh
-# else, continue
+# Main section of the script
+function main {
 
-if ! is_zsh_installed; then
-    ask_install_zsh
-else
-    echo "[INF] zsh is installed"
-    echo "[INF] OSTYPE was detected as $OSTYPE"
-fi
+    # change directory to ~
+    cd ~
 
-clone_if_not_exists "${CUSTOM_ZSH}/plugins/zsh-autosuggestions" "https://github.com/zsh-users/zsh-autosuggestions"
-clone_if_not_exists "${CUSTOM_ZSH}/plugins/zsh-syntax-highlighting" "https://github.com/zsh-users/zsh-syntax-highlighting.git"
-clone_if_not_exists "${HOME_ZSH}/themes/powerlevel10k" "https://github.com/romkatv/powerlevel10k.git"
-# add more plugins here ...
+    # Check if zsh is installed
+    if ! is_zsh_installed; then
+        ask_install_zsh
+    else
+        echo "[INF] zsh is installed, proceeding with OSTYPE detection"
+        echo "[INF] OSTYPE was detected as $OSTYPE"
+    fi
 
-echo "[INF] running zsh..."
+    # Clone repositories
+    clone_if_not_exists "${CUSTOM_ZSH}/plugins/zsh-autosuggestions" "https://github.com/zsh-users/zsh-autosuggestions"
+    clone_if_not_exists "${CUSTOM_ZSH}/plugins/zsh-syntax-highlighting" "https://github.com/zsh-users/zsh-syntax-highlighting.git"
+    clone_if_not_exists "${HOME_ZSH}/themes/powerlevel10k" "https://github.com/romkatv/powerlevel10k.git"
 
-zsh
+    echo "[INF] Done"
+    zsh
+}
 
-echo "[INF] done"
+# Call the main function
+main
